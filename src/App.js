1664b84c944f08c978da2db642f7c6846a7bf850
themarcBlanchard <marcblanchard82@gmail.com>
2022-09-01 17:49:16 -0400

    Log 1: I have no idea what I'm doing apart from CSS. I changed it to fit me. I added the button and the events for when a user is interacting with the app. The logic is up next, and it's in React. Not familliar with it but will be when creating my own based on this tutorial.

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useReducer} from 'react';
+import './styles.css';
+
+
+function reducer(sate, dispatch){
+
+}
+
+function App(){
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer);
+  return(
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">123,456 *</div>
+        <div className="current-operand">123,456</div>
+      </div>
+      <button className="span-two" id="ac">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  )
+}
+
+export default App;
